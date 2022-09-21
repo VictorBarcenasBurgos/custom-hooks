dc49551117fa3c208da7036b306388aaff988bee
Víctor Bárcenas Burgos <Vbarcenas@tress.com.mx>
2022-09-21 07:17:56 -0700

    Borrando index y agregando useReducer

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,36 @@
+
+
+export const todoReducer = (initialState = [], action) => {
+
+
+    console.log(action);
+ 
+    switch (action.type){
+        case 'Add Todo':
+            return [...initialState, action.payload];
+
+        case 'Delete Todo':
+            return initialState.filter(todo => todo.id !== action.payload );
+
+        case 'Toggle Todo':
+            return initialState.map( todo => {
+
+                if(todo.id === action.payload)
+                {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo;
+            });
+
+        default:
+            return initialState;
+
+
+    }
+
+    return initialState;
+}
